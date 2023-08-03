02502cbad93c0562822f0e8f0c1f8671f90094eb
Sabbat10 <sabbatlumpatshia99@gmail.com>
2023-08-03 11:33:19 +0100

    add message in file index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-const na;
+const message = [
+    ["Que dit un informaticien quand il s'ennuie ? Je m'en fiche"],
+    ["Que dit une noisette quand elle tombe dans l’eau ? Je me noix."],
+    [" Comment est-ce que les abeilles communiquent entre elles ? Par -miel."],
+    ["Quel est l’arbre préféré du chômeur ? Le bouleau."],
+    ["Que dit une mère à son fils geek quand le dîner est servi ? Alt Tab !"]
+]
